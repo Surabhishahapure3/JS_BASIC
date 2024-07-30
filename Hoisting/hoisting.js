c405056d032840e103a9984974b182deb8030f5c
Surabhishahapure3 <121013210+Surabhishahapure3@users.noreply.github.com>
2024-07-31 04:06:22 +0530

    Add files via upload

---
+++ Hoisting/hoisting.js
@@ -0,0 +1,23 @@
+// In variables it shows undefined
+console.log(x); // Output: undefined
+var x = 5;
+
+// in function it is opposite it will print the function 
+getname1;
+
+function getname1()
+{
+    console.log("hello")   //output: prints the struture of of function 
+}
+
+getname;
+var getname = ()=>
+{
+    console.log("hello")     // Output: gives an error, In the memory it show the (getname: undefined). It acts like a variable
+}
+
+console.log(a)
+var a = function()
+{
+    console.log("hello") // output: here also it acts like a variable so the answer would be the undefined
+}
