3bb944ee7f2847ab3f4c29a95efbadb713225740
Surabhishahapure3 <121013210+Surabhishahapure3@users.noreply.github.com>
2024-07-31 10:47:55 +0530

    Add files via upload

---
+++ synchronous_asynchronous/sync_async.js
@@ -0,0 +1,29 @@
+/*********SYNCHROUNOUS*************** */
+// console.log("hello")
+// console.log("IPS")
+// console.log("MBA")
+
+
+
+/**ASYNCHRONOUS*************** */
+console.log("hello")
+setTimeout(()=>{
+    console.log("IPS")
+},3000);
+console.log("MBA")
+
+
+// Synchronous JavaScript:
+// 1. Instruction in synchronous code executes in a given sequence.
+// 2. Each operation waits for the previous operation to complete its execution.
+// 3. Most of the time JavaScript is used as Synchronous code.
+
+// Asynchronous JavaScript:
+// 1. Instructions in asynchronous code can execute in parallel.
+// 2. Next operation can occur while the previous operation is still getting processed.
+// 3. Asynchronous JavaScript is preferred in situations in which execution gets blocked indefinitely.
+
+
+// what's github 
+// cancelIdleCallback,promise,async and await 
+// each 5 examples
